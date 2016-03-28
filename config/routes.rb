ae5607e6779965854d656f28302652c38ca849ef
Auroratide <tf.auroratide@gmail.com>
2016-03-28 16:51:22 +0000

    deploy script

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,11 @@ Rottenpotatoes::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :movies
+  resources :movies do
+    member do
+      get 'bydirectors'
+    end
+  end
 
   # Sample resource route with options:
   #   resources :products do
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe MoviesController do
+    before(:all) do
+        #@fake_movie = mock(:id => 1)
+    end
     
+    it 'should call movies_with_same_director' do
+        get :bydirectors
+    end
 end
